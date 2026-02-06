9798d0a6d7898276793cc7e10f07f434c628ac09
shree <159869108+shreeharinik@users.noreply.github.com>
2026-02-06 10:28:48 +0000

    Refactor output variables in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,4 @@
-output "web_ips" {
-  value = aws_instance.web[*].public_ip
-}
-
-output "alb_dns" {
-  value = aws_lb.alb.dns_name
+output "web_public_ips" {
+  description = "Public IPs of web EC2 instances"
+  value       = aws_instance.web[*].public_ip
 }
