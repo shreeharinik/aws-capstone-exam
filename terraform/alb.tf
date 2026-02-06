aae7b42a35a8df6b128f212d831a340ce4875a23
shree <159869108+shreeharinik@users.noreply.github.com>
2026-02-06 09:02:42 +0000

    Add AWS Application Load Balancer configuration

---
+++ terraform/alb.tf
@@ -0,0 +1,30 @@
+resource "aws_lb" "alb" {
+  name               = "streamline-alb"
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public[*].id
+  security_groups    = [aws_security_group.web_sg.id]
+}
+
+resource "aws_lb_target_group" "tg" {
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "attach" {
+  count            = 2
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.web[count.index].id
+  port             = 80
+}
