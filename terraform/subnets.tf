44366323814177653c6544cfd2da3ce2bcea8b6a
shree <159869108+shreeharinik@users.noreply.github.com>
2026-02-06 08:59:57 +0000

    Add public and private subnets in Terraform

---
+++ terraform/subnets.tf
@@ -0,0 +1,14 @@
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index + 1)
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet("10.0.0.0/16", 8, count.index + 3)
+  availability_zone = element(["us-east-1a", "us-east-1b"], count.index)
+}
